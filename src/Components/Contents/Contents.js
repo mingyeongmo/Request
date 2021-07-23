e94249814e7a38107d14466f590063a946c2e77c
mingyeongmo <mingyeongmp@naver.com>
2021-07-23 23:32:07 +0900

    귀찮..

---
+++ src/Components/Contents/Contents.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Contents = () => {
+    return (
+        <div className="main_contents">
+            <div className="main_slider">
+                
+            </div>
+        </div>
+    );
+};
+
+export default Contents;
---
+++ src/Components/Header/Header.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import './header.css';
+
+const Header = () => {
+    return (
+        <header className="header">
+            <div className="head">
+                <h1 className="logo">
+                    <a href="#"><img src={require("../../Asset/logo.png").default} /></a>
+                </h1>
+
+                <div className="List">
+                    <ul>
+                        <li><a href="#">사업소개</a></li>
+                        <li><a href="#">프리미엄</a></li>
+                        <li><a href="#">입지환경</a></li>
+                        <li><a href="#">단지안내</a></li>
+                        <li><a href="#">타입안내</a></li>
+                        <li><a href="#">뉴스정보</a></li>
+                    </ul>
+                </div>
+            </div>
+        </header>
+    );
+};
+
+export default Header;
